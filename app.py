3a117e82e5cd74069ec08590ab35be0cfd80f2a8
Samuel Ephrim <ephrims5@gmail.com>
2026-06-19 06:12:09 +0000

    updated with database

--- app.py
+++ app.py
@@ -1,10 +1,31 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
+from flask_mysqldb import MySQL
+import datetime
 
 app = Flask(__name__)
 
-@app.route('/')
+app.config['MYSQL_HOST'] = 'Localhost'
+app.config['MYSQL_DB'] = 'dictionary'
+app.config['MYSQL_USER'] = 'ephrim'
+app.config['MYSQL_PASSWORD'] = 'The1man@'
+
+mysql = MySQL(app)
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    user_response = ''
+    if request.method == 'POST':
+        conn = mysql.connection
+        cur = conn.cursor()
+        word = request.form['word']
+        cur.execute('select meaning from word where word=%s', (word,))
+        rv = cur.fetchall()
+        print(rv)
+        if rv:
+            user_response = f"{word}: \n{rv[0][0]}"
+        else:
+            user_response = f" sorry \''{word}\'' meaning was not found"
+    return render_template('index.html', user_response = user_response)
 
 @app.route('/dashboard')
 def dashboard():
